ef430f93044d977444d0d449b95a8a2159e3443c
KD-MM2 <57068549+KD-MM2@users.noreply.github.com>
2023-05-28 22:57:36 +0900

    Update markTransactionDone.js

--- markTransactionDone.js
+++ markTransactionDone.js
@@ -1,11 +1,11 @@
-var body = $response.body;
-const status = 'HTTP/1.1 200 OK';
+var newBody = $response.body;
+const newStatus = 'HTTP/1.1 200 OK';
 
 if ($prefs.valueForKey("regrant") == "huntroyale") {
-  body =
+  newBody =
     '<?xml version="1.0" encoding="UTF-8" standalone="yes"?> <plist version="1.0"> <dict> <key>pings</key> <array> </array> <key>jingleDocType</key> <string>inAppSuccess</string> <key>download-queue-item-count</key> <integer>0</integer> <key>jingleAction</key> <string>inAppTransactionDone</string> <key>dsid</key> <string>16221907643</string> <key>app-list</key> <array> </array> </dict> </plist>';
   console.log('triggered markTransactionDone body');
 }
 
 
-$done({status, body });
+$done({status: newStatus, body: newBody });
