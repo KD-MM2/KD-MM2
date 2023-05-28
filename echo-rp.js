767218142d9d13655d311020ddf31520beb4fbe0
KD-MM2 <57068549+KD-MM2@users.noreply.github.com>
2023-05-28 23:56:05 +0900

    Update echo-rp.js

--- echo-rp.js
+++ echo-rp.js
@@ -1,9 +1,14 @@
 const myStatus = "HTTP/1.1 200 OK";
 const myData = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?> <plist version="1.0"> <dict> <key>pings</key> <array> </array> <key>jingleDocType</key> <string>inAppSuccess</string> <key>download-queue-item-count</key> <integer>0</integer> <key>jingleAction</key> <string>inAppTransactionDone</string> <key>dsid</key> <string>16221907643</string> <key>app-list</key> <array> </array> </dict> </plist>';
+try {
+    var modifiedHeaders = $response.headers;
+} catch (e) {
+    console.log(e)
+}
 
 const myResponse = {
     status: myStatus,
-    body: myData // Optional.
+    body: myData
 };
 
 $done(myResponse);
