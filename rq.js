6e2640688fd44a31f9295b9ac813efed43b9211e
KD-MM2 <57068549+KD-MM2@users.noreply.github.com>
2023-05-28 22:56:56 +0900

    Update rq.js

--- rq.js
+++ rq.js
@@ -1,7 +1,7 @@
 const newBody = $request.body;
 const newStatus = 'HTTP/1.1 200 OK';
 
-if ($request.newBody.match(/com.hunt.royale/)) {
+if ($request.body.match(/com.hunt.royale/)) {
   $prefs.setValueForKey("huntroyale", "regrant");
   console.log('triggered huntroyale request');
 }
