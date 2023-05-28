547b950de5b0a71bcd3cbfff5ee02a4b0159c32d
KD-MM2 <57068549+KD-MM2@users.noreply.github.com>
2023-05-28 23:42:09 +0900

    Create echo-rp.js

---
+++ echo-rp.js
@@ -0,0 +1,9 @@
+const myStatus = "HTTP/1.1 200 OK";
+const myData = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?> <plist version="1.0"> <dict> <key>pings</key> <array> </array> <key>jingleDocType</key> <string>inAppSuccess</string> <key>download-queue-item-count</key> <integer>0</integer> <key>jingleAction</key> <string>inAppTransactionDone</string> <key>dsid</key> <string>16221907643</string> <key>app-list</key> <array> </array> </dict> </plist>';
+
+const myResponse = {
+    status: myStatus,
+    body: myData // Optional.
+};
+
+$done(myResponse);
