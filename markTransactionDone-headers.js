8e39d4560abace954e66e24396db7b0b2dc64462
KD-MM2 <57068549+KD-MM2@users.noreply.github.com>
2023-05-28 23:32:37 +0900

    Update markTransactionDone-headers.js

--- markTransactionDone-headers.js
+++ markTransactionDone-headers.js
@@ -1,2 +1,5 @@
-const newStatus = 'HTTP/1.1 200 OK';
-$done({status: newStatus});
+var modifiedHeaders = $response.headers;
+
+var modifiedStatus = 'HTTP/1.1 200 OK';
+
+$done({status: modifiedStatus, headers : modifiedHeaders});
